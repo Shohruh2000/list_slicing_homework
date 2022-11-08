40707c2e8d8d680bec3a631194fdfe8d60d29669
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-08 19:59:19 +0500

    Slicing_list09.py

--- Slicing_list09.py
+++ Slicing_list09.py
@@ -6,6 +6,9 @@ def main(list1,n):
         n(int): parameter
     Returns:
         list: return answer.
-    """  
-    a = len(list1)
-    return list[n:a]
+    """
+    a = list1
+    return (a[n:])
+print(main(['a', 'b', 'c', 'd', 'e', 'f'],2))
+
+    
